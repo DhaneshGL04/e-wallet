680502710f7cd699efa9b7ba52c6162371958360
Nuthan f f <novopad4d@gmail.com>
2025-06-02 07:35:11 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/github/Dhanesh/config/SecurityConfig.java
+++ backend/src/main/java/com/github/Dhanesh/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
+import org.springframework.context.annotation.Bean;
 import java.util.Arrays;
 import java.util.Collections;
 
